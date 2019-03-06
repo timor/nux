0982184bbfc0868dfa19513afed992a1d587be56
timor <timor.dd@googlemail.com>
2019-03-06 15:51:38 +0100

    add default.nix for easy local builiding

---
+++ default.nix
@@ -0,0 +1,29 @@
+with import <nixpkgs> { };
+
+stdenv.mkDerivation rec {
+  name = "nux-${version}";
+  version = "1.0.0";
+
+  src = ./.;
+
+  buildCommand = ''
+  mkdir -p $out/bin
+  cp $src/nux $out/bin/nux
+  chmod +x $out/bin/nux
+'';
+
+  meta = with stdenv.lib; {
+    description = "Imperative-Declarative alternative to nix-env";
+    longDescription = ''
+      Used to maintain a set of packages in the user environment, the main
+      difference to nix-env is that updates are tracked based on path names
+      defined in $NIX_PATH and attribute names instead of the name attribute of
+      derivations.  Consequently, the package attributes are the same as the
+      ones returned by `nix search`.  Based on the following original code:
+      https://gist.github.com/danbst/f07d9760ed15dd844e141177cf7dd478 by danbst.
+  '';
+    maintainers = [ maintainers.timor ];
+    platforms = platforms.all;
+    license = licenses.unlicense;
+  };
+}
